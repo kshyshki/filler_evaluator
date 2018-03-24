673e9bfb9c7729431f068acb7b3dfdfce3e9e18b
wixa <sobaka-borabaka@bk.ru>
2018-03-24 12:12:21 +0200

    fix problam with start

--- play_with_all.sh
+++ play_with_all.sh
@@ -4,23 +4,11 @@ then
     printf "\033[0;31mIMPORTANT\033[0m: .filler - must present in name of your player\n"
     printf "\033[0;32mEXAMPLE\033[0m: sh play_with_all.sh maps/map00 players/carli.filler\n"
 else
-    if [ -z "$2" ];
-    then
-        printf "\033[0;32mUSAGE\033[0m: sh play_with_all.sh [path to your map] [path to your player.filler]\n"
-        printf "\033[0;31mIMPORTANT\033[0m: .filler - must present in name of your player\n"
-        printf "\033[0;32mEXAMPLE\033[0m: sh play_with_all.sh maps/map00 players/carli.filler\n"
-    else
-        if grep -q ".filler" "$2" ;
-        then
-            sh tests.sh players/carli.filler $1 $2
-            sh tests.sh players/superjeannot.filler $1 $2
-            sh tests.sh players/abanlin.filler $1 $2
-            sh tests.sh players/champely.filler $1 $2
-            sh tests.sh players/grati.filler $1 $2
-            sh tests.sh players/hcao.filler $1 $2
-        else
-            printf "%s\n" "Not valid arguments, please check!"
-        fi
-    fi
+    sh tests.sh players/carli.filler $1 $2
+    sh tests.sh players/superjeannot.filler $1 $2
+    sh tests.sh players/abanlin.filler $1 $2
+    sh tests.sh players/champely.filler $1 $2
+    sh tests.sh players/grati.filler $1 $2
+    sh tests.sh players/hcao.filler $1 $2
 fi
 
